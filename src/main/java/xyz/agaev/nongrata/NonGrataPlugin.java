20d11f424408de9c1b6e0cbfedd055082dd90519
laptopacadapterquality-web <laptopacadapter.quality@gmail.com>
2026-07-11 12:54:24 +0500

    Update NonGrataPlugin.java

--- src/main/java/xyz/agaev/nongrata/NonGrataPlugin.java
+++ src/main/java/xyz/agaev/nongrata/NonGrataPlugin.java
@@ -1,1 +1,35 @@
+package xyz.agaev.nongrata;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NonGrataPlugin extends JavaPlugin {
+
+    private static NonGrataPlugin instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        saveDefaultConfig();
+        saveResource("messages.yml", false);
+
+        getLogger().info("================================");
+        getLogger().info(" NonGrata v1.0 enabled");
+        getLogger().info(" LiteBans integration loaded");
+        getLogger().info("================================");
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("NonGrata disabled.");
+
+    }
+
+    public static NonGrataPlugin getInstance() {
+        return instance;
+    }
+
+}
