05d52d99712c0148d764b1cdd42969431caea5b9
laptopacadapterquality-web <laptopacadapter.quality@gmail.com>
2026-07-11 12:56:56 +0500

    Create PlayerRecord.java

---
+++ src/main/java/xyz/agaev/nongrata/model/PlayerRecord.java
@@ -0,0 +1,74 @@
+package xyz.agaev.nongrata.model;
+
+import java.util.UUID;
+
+public class PlayerRecord {
+
+    private UUID uuid;
+    private String nickname;
+    private String category;
+    private int danger;
+    private String reason;
+    private String author;
+    private long createdAt;
+
+    public PlayerRecord() {
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDanger() {
+        return danger;
+    }
+
+    public void setDanger(int danger) {
+        this.danger = danger;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
